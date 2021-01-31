2534478df7ccbe969c6b9a0da163138597068766
Monkey-X <715580162@qq.com>
2021-01-31 16:37:18 +0800

    每一条记录显示包含“合同备案序号：xxxx/项目编号：xxxxxx"和”申请时间

--- app/src/main/java/com/etek/controller/adapter/ContractAdapter.java
+++ app/src/main/java/com/etek/controller/adapter/ContractAdapter.java
@@ -1,6 +1,7 @@
 package com.etek.controller.adapter;
 
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -8,6 +9,12 @@ import com.chad.library.adapter.base.BaseViewHolder;
 import com.etek.controller.R;
 import com.etek.controller.dto.ProjectDownLoadEntity;
 import com.etek.controller.persistence.entity.ProjectInfoEntity;
+import com.etek.sommerlibrary.utils.DateUtil;
+import com.google.gson.Gson;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import cz.msebera.android.httpclient.util.TextUtils;
@@ -20,14 +27,30 @@ public class ContractAdapter extends BaseQuickAdapter<ProjectInfoEntity, BaseVie
 
     @Override
     protected void convert(BaseViewHolder helper, ProjectInfoEntity item) {
-        String contractName = item.getContractName();
-        String contractCode = item.getContractCode();
-        if (!TextUtils.isEmpty(contractName) || !TextUtils.isEmpty(contractCode)){
-            ((TextView) helper.getView(R.id.contract_name)).setText(contractName);//合同名称
-            ((TextView) helper.getView(R.id.contract_code)).setText(contractCode);//合同编号
+        String proCode = item.getProCode();
+        String contractCode = item.getContractCode();// 合同编号
+        Date createTime = item.getCreateTime();
+
+        if (!TextUtils.isEmpty(contractCode)) {
+            ((TextView) helper.getView(R.id.contract_code)).setText("合同备案序号："+contractCode);
         }else{
-            ((TextView) helper.getView(R.id.contract_name)).setText(item.getProName());//合同名称
-            ((TextView) helper.getView(R.id.contract_code)).setText(item.getProCode());//合同编号
+            ((TextView) helper.getView(R.id.contract_code)).setVisibility(View.GONE);
         }
+
+
+        if (!TextUtils.isEmpty(proCode)) {
+            ((TextView) helper.getView(R.id.contract_name)).setText("项目编号："+proCode);
+        }else{
+            ((TextView) helper.getView(R.id.contract_name)).setVisibility(View.GONE);
+        }
+
+        // 创建时间
+        if (createTime !=null) {
+            String dateStr = DateUtil.getDateStr(createTime);
+            ((TextView) helper.getView(R.id.create_time)).setText(dateStr);//创建时间
+        }else{
+            ((TextView) helper.getView(R.id.create_time)).setVisibility(View.GONE);
+        }
+
     }
 }
