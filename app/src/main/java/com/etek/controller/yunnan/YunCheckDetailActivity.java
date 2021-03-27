533e9a73c9d58560c77ac4b830f1269d43d19d99
Monkey-X <715580162@qq.com>
2021-03-27 23:50:03 +0800

    云南项目的规则检查

---
+++ app/src/main/java/com/etek/controller/yunnan/YunCheckDetailActivity.java
@@ -0,0 +1,18 @@
+package com.etek.controller.yunnan;
+
+import android.os.Bundle;
+
+import com.etek.controller.R;
+import com.etek.controller.activity.BaseActivity;
+
+/**
+ * 云南项目的规则检查
+ */
+public class YunCheckDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yun_check_detail);
+    }
+}
