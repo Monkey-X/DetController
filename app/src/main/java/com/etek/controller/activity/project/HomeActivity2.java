1f6e656c8e41d4723e8cfc2d60fbad6733d5aa2c
XIN HONWAY <hongwei.xin@avantport.com>
2021-03-30 17:38:14 +0800

    【主界面】，下载失败报错

--- app/src/main/java/com/etek/controller/activity/project/HomeActivity2.java
+++ app/src/main/java/com/etek/controller/activity/project/HomeActivity2.java
@@ -9,12 +9,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
@@ -259,6 +261,10 @@ public class HomeActivity2 extends BaseActivity implements ActivityCompat.OnRequ
                             }
                         });
                         Log.e(TAG, "Exception: " + e.getMessage());
+
+                        Looper.prepare();
+                        Toast.makeText(mContext, "下载失败，请检查网络!", Toast.LENGTH_SHORT).show();
+                        Looper.loop();
                     }
                 });
             }
